e619a4e6261e3f6949866486cd970e7b4685cb2b
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-03-27 19:08:47 +0100

    Added the second api to app.js. There will be playlist and activities proposed based on the summary of the weather

--- lab4-api/app.js
+++ lab4-api/app.js
@@ -29,7 +29,7 @@ class App{
                 return response.json();
             }).then(data => {
                 localStorage.setItem('data',JSON.stringify(data))
-                console.log(data);
+                
                 document.querySelector(".weatherInfo .temp").innerHTML = data.currently.temperature + "°";
                 document.querySelector(".weatherInfo .summary").innerHTML = data.currently.summary;
                 this.changeElement(data.currently.icon);
@@ -40,7 +40,7 @@ class App{
             try{
                 let data = JSON.parse(localStorage.getItem('data'));
                 document.querySelector(".weatherInfo .temp").innerHTML = data.currently.temperature + "°";
-                console.log(data)
+                
                 document.querySelector(".weatherInfo .temp").innerHTML = data.currently.temperature + "°";
                 document.querySelector(".weatherInfo .summary").innerHTML = data.currently.summary;
                 this.changeElement(data.currently.icon);
@@ -65,25 +65,49 @@ class App{
         switch(icon){
             case "clear-day":
                 document.querySelector(".weather--icon").className = "far fa-sun weather--icon";
+                this.getActivity("music","");
+                document.querySelector(".songPic").src = "https://i.scdn.co/image/ab67706f00000002fe0099a8dcd3054706ffc92f"
                 break;
             case "clear-night":
                 document.querySelector(".weather--icon").className = "far fa-moon weather--icon";
+                this.getActivity("recreational"," while listening to this playlist");
+                document.querySelector(".songPic").src = "https://pl.scdn.co/images/pl/default/79f1ecd290c813a4deff9439f9d045e84dc76626"
                 break;
             case "rain":
                 document.querySelector(".weather--icon").className = "fas fa-cloud-rain weather--icon";
+                this.getActivity("relaxation","while enjoying this playlist!");
+                document.querySelector(".songPic").src = "https://pl.scdn.co/images/pl/default/https://i.scdn.co/image/ab67706f00000002a692c498201090e78f8c9f7e"
                 break;
             case "snow":
                 document.querySelector(".weather--icon").className = "far fa-snowflake weather--icon";
+                this.getActivity("social","while singing with this playlist");
+                document.querySelector(".songPic").src = "https://i.scdn.co/image/ab67616d0000b27381af630ac06f82c24d8f85ee";
                 break;
             case "cloudy":
                 document.querySelector(".weather--icon").className = "fas fa-cloud weather--icon";
+                this.getActivity("cooking","moving to this beat");
+                document.querySelector(".songPic").src = "https://i.scdn.co/image/ab67706f0000000272d1ee442828718107cb4fb6";
                 break;
             case "wind":
                 document.querySelector(".weather--icon").className = "fas fa-wind weather--icon";
+                this.getActivity("diy"," while dancing to this playlist");
+                document.querySelector(".songPic").src = "https://i.scdn.co/image/ab67706f000000025af1070c80cd50dbbb4cfa19";
                 break;
             default:
                 document.querySelector(".weather--icon").className = "fas fa-cloud-sun weather--icon";
+                document.querySelector(".songPic").src = "https://i.scdn.co/image/ab67706f00000002cef1e1e83454017583e8bd8f";
         }
+    }
+
+    getActivity(type,extra){
+        let url = `https://cors-anywhere.herokuapp.com/https://www.boredapi.com/api/activity?type=${type}`;
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            document.querySelector(".activity").innerHTML = data.activity + " " + extra;
+        }).catch(err =>{
+            console.log(err);
+        });
     }
 }
 
